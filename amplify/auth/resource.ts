a899dd65266177a381f19caeaf53c6ea3d9771de
ZArihab <165288141+ZArihab@users.noreply.github.com>
2026-03-07 22:15:57 +0000

    Update resource.ts

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -13,8 +13,8 @@ export const auth = defineAuth({
         clientSecret: secret('GOOGLE_CLIENT_SECRET'),
         scopes: ['email']
       },
-      callbackUrls: ["myapp://"],
-      logoutUrls: ["myapp://"],
+      callbackUrls: ["myapp://callback"],
+      logoutUrls: ["myapp://signout"],
     }
   }
 });
